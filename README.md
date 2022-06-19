e197988f2c2a0e7932a28482d1f49d76356dbce6
Akshaye Shenoi <akshaye.shenoi@gmail.com>
2022-06-19 15:45:30 +0800

    hello hn

---
+++ README.md
@@ -0,0 +1,26 @@
+# HN Navigate Pro
+
+No frills Hackernews extension that enables [RES](https://redditenhancementsuite.com/) like keyboard navigation.
+
+- Navigate with `j`, `k` keys
+- `Enter` to open selected link
+- `c` to open comments
+
+
+## Installation
+1. Clone the repository.
+
+2. Manually load the extension by following these steps (from [Chrome Developers](https://developer.chrome.com/docs/extensions/mv3/getstarted/#unpacked)):
+> 1. Open the Extension Management page by navigating to chrome://extensions.
+> - Alternatively, open this page by clicking on the Extensions menu button and selecting Manage Extensions at the bottom of the menu.
+> - Alternatively, open this page by clicking on the Chrome menu, hovering over More Tools then selecting Extensions
+>
+> 2. Enable Developer Mode by clicking the toggle switch next to Developer mode.
+> 4. Click the Load unpacked button and select the extension directory.
+
+3. Navigate to [Hackernews](https://news.ycombinator.com/)!
+
+### Todo
+- Navigate comments
+- Quick shortcuts to New, Ask, Show, Jobs, etc.
+- Upvote selected link/comment
---
+++ content-script.js
@@ -0,0 +1,110 @@
+const athings = document.querySelectorAll("table.itemlist tr.athing")
+let athingSelectedIndex = -1;
+
+document.addEventListener('keypress', detectKey);
+
+function detectKey(key) {
+    if (!['KeyJ', 'KeyK', 'Enter', 'KeyC'].includes(key.code)) return;
+
+    if (athingSelectedIndex == -1) {
+        // select first athing
+        athingSelectedIndex = 0;
+        addbox(athings[athingSelectedIndex]);
+
+        return;
+    }
+
+    if (key.code == 'KeyJ') {
+        // go down
+        if (athingSelectedIndex >= athings.length - 1) return;
+        // remove older selected athing
+        removebox(athings[athingSelectedIndex]);
+
+        // select next athing
+        athingSelectedIndex++;
+        addbox(athings[athingSelectedIndex]);
+
+        scrollDownIfNecessary();
+
+        return;
+    }
+    
+
+    if (key.code == 'KeyK') {
+        // go up
+        if (athingSelectedIndex <= 0) return;
+        // remove older selected athing
+        removebox(athings[athingSelectedIndex]);
+
+        // select previous athing
+        athingSelectedIndex--;
+        addbox(athings[athingSelectedIndex]);
+
+        scrollUpIfNecessary();
+
+        return;
+    }
+
+    if (key.code == 'Enter') {
+        // open link
+        // new tab if shift is pressed
+        // todo:^ (shift seems to override click behaviour)
+        // skipping feature for now
+        if (athingSelectedIndex == -1) return;
+
+        const link = athings[athingSelectedIndex].querySelector("td:nth-child(3) a");
+        window.open(link.href, "_blank");
+
+        return;
+    }
+
+    if (key.code == 'KeyC') {
+        // open comments
+        if (athingSelectedIndex == -1) return;
+
+        // first select next sibling (it's not nested for some reason)
+        const athingSubtext = athings[athingSelectedIndex].nextElementSibling;
+        const commentLink = athingSubtext.querySelector("td.subtext > a:nth-child(6)");
+
+        window.open(commentLink.href, "_blank");
+
+        return;
+    }
+
+}
+
+function scrollDownIfNecessary() {
+    // get height and y coord of element (including the subtext, which is the next sibling element)
+    const height = athings[athingSelectedIndex].getBoundingClientRect().height
+                    + athings[athingSelectedIndex].nextElementSibling.getBoundingClientRect().height;
+
+    const bottom = athings[athingSelectedIndex].nextElementSibling.getBoundingClientRect().bottom;
+
+    if (bottom + 10 > window.innerHeight) {
+        // scroll by one box amount
+        window.scroll(0, window.scrollY + height + 10);
+    }
+}
+
+function scrollUpIfNecessary() {
+    // get height and y coord of element (including the subtext, which is the next sibling element)
+    const top = athings[athingSelectedIndex].getBoundingClientRect().top;
+
+    const height = athings[athingSelectedIndex].getBoundingClientRect().height
+                    + athings[athingSelectedIndex].nextElementSibling.getBoundingClientRect().height;
+
+    if (top - 10 < 0) {
+        // scroll by one box amount
+        window.scroll(0, window.scrollY - (height + 10));
+    }
+}
+
+function addbox(athing) {
+    // adds box around link title
+    athing.querySelector("td:nth-child(3)").style['border'] = "2px #4848dc solid";
+}
+
+function removebox(athing) {
+    // removes box around link title
+    athing.querySelector("td:nth-child(3)").style['border'] = "none";
+}
